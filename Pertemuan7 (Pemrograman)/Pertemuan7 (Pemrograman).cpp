f3decf536f9b7b13f0e1ef897488da429ad6f39c
ASUS <faris13abiyu@gmail.com>
2024-04-02 20:42:26 +0700

    Add project files.

---
+++ Pertemuan7 (Pemrograman)/Pertemuan7 (Pemrograman).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+struct mahasiswa {
+	string nim;
+	string nama;
+	string alamat;
+	string umur;
+};
+
+int main(){
+	mahasiswa mhs;
+	cout << "Nomor Mahasiswa : ";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	cin >> mhs.nama;
+	cout << "Alamat Mahasiswa : ";
+	cin >> mhs.alamat;
+	cout << "Umur Mahasiswa : ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n Nim :" << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n alamat : " << mhs.alamat;
+	cout << "\n Umur : " << mhs.umur;
+
+} 
+
